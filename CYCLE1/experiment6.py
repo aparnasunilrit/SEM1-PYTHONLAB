beecd03229c1454fe35dbc5889f82188e5e9e4d0
APARNA SUNIL T P <24mp2314@rit.ac.in>
2024-11-29 13:40:41 +0530

    Create experiment6.py

---
+++ CYCLE1/experiment6.py
@@ -0,0 +1,6 @@
+//aim Write a Python program to get a string which is n (non-negative integer) copies of a given string.
+
+name=input("enter the string:")
+n=int(input("enter the number of copies:"))
+c= name*n
+print(f"copies of the string are : {c}")
