893a4f5d7ba51dddb87346fa4595190381c83251
APARNA SUNIL T P <24mp2314@rit.ac.in>
2024-11-29 13:40:03 +0530

    Create experiment5.py

---
+++ CYCLE1/experiment5.py
@@ -0,0 +1,13 @@
+//aim Write a Python program to perform arithmetic operations on two integer numbers.
+
+num1=int(input("enter first number:"))
+num2=int(input("enter second number:"))
+sum=num1+num2
+difference=num1-num2
+product=num1*num2modulus=num1%num2
+division=num1/num2
+print(f"sum: {num1}+{num2}={sum}")
+print(f"difference: {num1}-{num2}={difference}")
+print(f"product:{num1}*{num2}={product}")
+print(f"division:{num1}/{num2}={division}")
+print(f"modulus:{num1}%{num2}={modulus}")
