4e376133cc2177da48649a3f89e13f6de3037ac4
APARNA SUNIL T P <24mp2314@rit.ac.in>
2024-11-29 13:38:34 +0530

    Create experiment3.py

---
+++ CYCLE1/experiment3.py
@@ -0,0 +1,7 @@
+//aim Write a program to demonstrate different number data types in python?
+
+int_num=10
+print(f"integer: {int_num},type: {type(int_num)}")
+float=10.5print(f"float: {float},type: {type(float)}")
+complex_num=3+5j
+print(f"complex number: {complex_num},type: {type(complex_num)}")
