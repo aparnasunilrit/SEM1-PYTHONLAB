556cd5efb91cf8add333a27d848f599ecd359b53
APARNA SUNIL T P <24mp2314@rit.ac.in>
2024-11-29 13:50:35 +0530

    Update experiment2.py

--- CYCLE1/experiment2.py
+++ CYCLE1/experiment2.py
@@ -1,4 +1,4 @@
-//aim Write a program to calculate the area of a circle by reading inputs from the user
+#aim Write a program to calculate the area of a circle by reading inputs from the user
 
 radius=float(input("enter the radius:"))
 area=3.14*radius*radius
