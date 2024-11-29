01fad375ec51f0f72536bb0cc1f5d92073346646
APARNA SUNIL T P <24mp2314@rit.ac.in>
2024-11-29 13:50:50 +0530

    Update experiment3.py

--- CYCLE1/experiment3.py
+++ CYCLE1/experiment3.py
@@ -1,4 +1,4 @@
-//aim Write a program to demonstrate different number data types in python?
+#aim Write a program to demonstrate different number data types in python?
 
 int_num=10
 print(f"integer: {int_num},type: {type(int_num)}")
