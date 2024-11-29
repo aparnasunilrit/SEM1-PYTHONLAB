030fc299b27e208e06bead91ae14ac49cf69a542
APARNA SUNIL T P <24mp2314@rit.ac.in>
2024-11-29 13:35:49 +0530

    Create experiment1.py

---
+++ CYCLE1/experiment1.py
@@ -0,0 +1,5 @@
+// Write a program that prompts the user to enter his first name and last name and thendisplays a message “Greetings!!! First name Last name”.
+
+first_name=input("enter the first name:")
+last_name=input("enter the last name")
+print(f"Greetings {first_name} {last_name}!")
