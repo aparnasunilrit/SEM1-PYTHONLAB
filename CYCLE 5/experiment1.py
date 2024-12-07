1ef15a4d27bf7d70bf63967caceedffefd12801a
APARNA SUNIL T P <24mp2314@rit.ac.in>
2024-12-07 12:43:44 +0530

    Update experiment1.py

--- CYCLE 5/experiment1.py
+++ CYCLE 5/experiment1.py
@@ -1,1 +1,11 @@
+import calendar
 
+def check_leap_year(year):
+    if calendar.isleap(year):
+        return f"{year} is a leap year."
+    else:
+        return f"{year} is not a leap year."
+
+year = int(input("Enter a year: "))
+
+print(check_leap_year(year))
