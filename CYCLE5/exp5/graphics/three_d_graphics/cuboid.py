4fa8d56aa08bcd9c5301e263263c59860dd467a7
APARNA SUNIL T P <24mp2314@rit.ac.in>
2024-12-07 15:51:45 +0530

    Add files via upload

---
+++ CYCLE5/exp5/graphics/three_d_graphics/cuboid.py
@@ -0,0 +1,9 @@
+
+
+def cuboidarea(length, width, height):
+    """Calculate the surface area of a cuboid."""
+    return 2 * (length * width + width * height + height * length)
+
+def cuboidvolume(length, width, height):
+    """Calculate the volume of a cuboid."""
+    return length * width * height
---
+++ CYCLE5/exp5/graphics/three_d_graphics/sphere.py
@@ -0,0 +1,10 @@
+
+import math
+
+def area(radius):
+    """Calculate the surface area of a sphere."""
+    return 4 * math.pi * radius ** 2
+
+def volume(radius):
+    """Calculate the volume of a sphere."""
+    return (4/3) * math.pi * radius ** 3
