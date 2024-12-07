8e63186107a364a80373779de18535007fa1ae9f
APARNA SUNIL T P <24mp2314@rit.ac.in>
2024-12-07 12:46:22 +0530

    Update experiment2.py

--- CYCLE 5/experiment2.py
+++ CYCLE 5/experiment2.py
@@ -7,3 +7,13 @@ def area(radius):
 def perimeter(radius):
     """Calculate the perimeter (circumference) of a circle."""
     return 2 * math.pi * radius
+
+
+
+def area(length, width):
+    """Calculate the area of a rectangle."""
+    return length * width
+
+def perimeter(length, width):
+    """Calculate the perimeter of a rectangle."""
+    return 2 * (length + width)
